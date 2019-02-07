22ae7d979e1103ad8989515143e90a884488f178
Meng To <mengto@Mengs-MacBook-Pro.local>
2019-02-07 14:02:35 -0500

    2. Styled Components

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components";
+import Card from "./components/Card";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>My first app!</Text>
-      </View>
+      <Container>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Title>Welcome back,</Title>
+          <Name>Meng</Name>
+        </TitleBar>
+        <Subtitle>Continue Learning</Subtitle>
+        <Card />
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
---
+++ components/Card.js
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = props => (
+  <Container>
+    <Cover>
+      <Image source={require("../assets/background2.jpg")} />
+      <Title>Styled Components</Title>
+    </Cover>
+    <Content>
+      <Logo source={require("../assets/logo-react.png")} />
+      <Wrapper>
+        <Caption>React Native</Caption>
+        <Subtitle>5 of 12 sections</Subtitle>
+      </Wrapper>
+    </Content>
+  </Container>
+);
+
+export default Card;
+
+const Content = styled.View`
+  padding-left: 20px;
+  flex-direction: row;
+  align-items: center;
+  height: 80px;
+`;
+
+const Logo = styled.Image`
+  width: 44px;
+  height: 44px;
+`;
+
+const Caption = styled.Text`
+  color: #3c4560;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  text-transform: uppercase;
+  margin-top: 4px;
+`;
+
+const Wrapper = styled.View`
+  margin-left: 10px;
+`;
+
+const Container = styled.View`
+  background: white;
+  width: 315px;
+  height: 280px;
+  border-radius: 14px;
+  margin-left: 20px;
+  margin-top: 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.15);
+`;
+
+const Cover = styled.View`
+  width: 100%;
+  height: 200px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+  width: 170px;
+`;
