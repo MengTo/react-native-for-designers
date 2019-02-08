3038c854983ff8488eed13e9516e38be005b0e7c
Meng To <mengto@mengs-mbp.lan>
2019-02-08 16:44:46 -0500

    4. Static Data and Loop

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Card from "./components/Card";
 import { Icon } from "expo";
 import { NotificationIcon } from "./components/Icons";
 import Logo from "./components/Logo";
+import Course from "./components/Course";
 
 export default class App extends React.Component {
   render() {
@@ -28,12 +29,11 @@ export default class App extends React.Component {
                 paddingTop: 30
               }}
               horizontal={true}
+              showsHorizontalScrollIndicator={false}
             >
-              <Logo
-                image={require("./assets/logo-framerx.png")}
-                text="Framer X"
-              />
-              <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+              {logos.map((logo, index) => (
+                <Logo key={index} image={logo.image} text={logo.text} />
+              ))}
             </ScrollView>
             <Subtitle>Continue Learning</Subtitle>
             <ScrollView
@@ -41,21 +41,30 @@ export default class App extends React.Component {
               style={{ paddingBottom: 30 }}
               showsHorizontalScrollIndicator={false}
             >
-              <Card
-                title="Styled Components"
-                image={require("./assets/background2.jpg")}
-                caption="React Native"
-                logo={require("./assets/logo-react.png")}
-                subtitle="5 of 12 sections"
-              />
-              <Card
-                title="Styled Components 2"
-                image={require("./assets/background1.jpg")}
-                caption="React Native"
-                logo={require("./assets/logo-react.png")}
-                subtitle="5 of 12 sections"
-              />
+              {cards.map((card, index) => (
+                <Card
+                  key={index}
+                  title={card.title}
+                  image={card.image}
+                  caption={card.caption}
+                  logo={card.logo}
+                  subtitle={card.subtitle}
+                />
+              ))}
             </ScrollView>
+            <Subtitle>Popular Courses</Subtitle>
+            {courses.map((course, index) => (
+              <Course
+                key={index}
+                image={course.image}
+                title={course.title}
+                subtitle={course.subtitle}
+                logo={course.logo}
+                author={course.author}
+                avatar={course.avatar}
+                caption={course.caption}
+              />
+            ))}
           </ScrollView>
         </SafeAreaView>
       </Container>
@@ -105,3 +114,101 @@ const TitleBar = styled.View`
   margin-top: 50px;
   padding-left: 80px;
 `;
+
+const logos = [
+  {
+    image: require("./assets/logo-framerx.png"),
+    text: "Framer X"
+  },
+  {
+    image: require("./assets/logo-figma.png"),
+    text: "Figma"
+  },
+  {
+    image: require("./assets/logo-studio.png"),
+    text: "Studio"
+  },
+  {
+    image: require("./assets/logo-react.png"),
+    text: "React"
+  },
+  {
+    image: require("./assets/logo-swift.png"),
+    text: "Swift"
+  },
+  {
+    image: require("./assets/logo-sketch.png"),
+    text: "Sketch"
+  }
+];
+
+const cards = [
+  {
+    title: "React Native for Designers",
+    image: require("./assets/background11.jpg"),
+    subtitle: "React Native",
+    caption: "1 of 12 sections",
+    logo: require("./assets/logo-react.png")
+  },
+  {
+    title: "Styled Components",
+    image: require("./assets/background12.jpg"),
+    subtitle: "React Native",
+    caption: "2 of 12 sections",
+    logo: require("./assets/logo-react.png")
+  },
+  {
+    title: "Props and Icons",
+    image: require("./assets/background13.jpg"),
+    subtitle: "React Native",
+    caption: "3 of 12 sections",
+    logo: require("./assets/logo-react.png")
+  },
+  {
+    title: "Static Data and Loop",
+    image: require("./assets/background14.jpg"),
+    subtitle: "React Native",
+    caption: "4 of 12 sections",
+    logo: require("./assets/logo-react.png")
+  }
+];
+
+const courses = [
+  {
+    title: "Prototype in InVision Studio",
+    subtitle: "10 sections",
+    image: require("./assets/background13.jpg"),
+    logo: require("./assets/logo-studio.png"),
+    author: "Meng To",
+    avatar: require("./assets/avatar.jpg"),
+    caption: "Design and interactive prototype"
+  },
+  {
+    title: "React for Designers",
+    subtitle: "12 sections",
+    image: require("./assets/background11.jpg"),
+    logo: require("./assets/logo-react.png"),
+    author: "Meng To",
+    avatar: require("./assets/avatar.jpg"),
+    caption: "Learn to design and code a React site"
+  },
+  {
+    title: "Design and Code with Framer X",
+    subtitle: "10 sections",
+    image: require("./assets/background14.jpg"),
+    logo: require("./assets/logo-framerx.png"),
+    author: "Meng To",
+    avatar: require("./assets/avatar.jpg"),
+    caption: "Create powerful design and code components for your app"
+  },
+  {
+    title: "Design System in Figma",
+    subtitle: "10 sections",
+    image: require("./assets/background6.jpg"),
+    logo: require("./assets/logo-figma.png"),
+    author: "Meng To",
+    avatar: require("./assets/avatar.jpg"),
+    caption:
+      "Complete guide to designing a site using a collaborative design tool"
+  }
+];
---
+++ components/Course.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+
+const Course = props => (
+  <Container>
+    <Cover>
+      <Image source={props.image} />
+      <Logo source={props.logo} resizeMode="contain" />
+      <Subtitle>{props.subtitle}</Subtitle>
+      <Title>{props.title}</Title>
+    </Cover>
+    <Content>
+      <Avatar source={props.avatar} />
+      <Caption>{props.caption}</Caption>
+      <Author>Taught by {props.author}</Author>
+    </Content>
+  </Container>
+);
+
+export default Course;
+
+const Container = styled.View`
+  width: 335px;
+  height: 335px;
+  background: white;
+  margin: 10px 20px;
+  border-radius: 14px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+`;
+
+const Cover = styled.View`
+  height: 260px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  overflow: hidden;
+  justify-content: flex-end;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Logo = styled.Image`
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  top: 90px;
+  left: 50%;
+  margin-left: -24px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: 600;
+  margin-top: 4px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  width: 170px;
+`;
+
+const Subtitle = styled.Text`
+  font-size: 15px;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.8);
+  text-transform: uppercase;
+  margin-left: 20px;
+`;
+
+const Content = styled.View`
+  padding-left: 62px;
+  justify-content: center;
+  height: 75px;
+`;
+
+const Avatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  border-radius: 16px;
+`;
+
+const Caption = styled.Text`
+  font-size: 14px;
+  color: #3c4560;
+  font-weight: 500;
+`;
+
+const Author = styled.Text`
+  font-size: 13px;
+  color: #b8bece;
+  font-weight: 500;
+  margin-top: 4px;
+`;
